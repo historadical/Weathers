c4a2ec76975d65f4b86ae4b31216cc42930015d7
Nic Laughter <nicholaslaughter@icloud.com>
2024-10-27 12:04:46 -0600

    Initial Commit

---
+++ Weathers/WeathersApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeathersApp.swift
+//  Weathers
+//
+//  Created by Nic on 10/27/24.
+//
+
+import SwiftUI
+
+@main
+struct WeathersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeathersTests/WeathersTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeathersTests.swift
+//  WeathersTests
+//
+//  Created by Nic on 10/27/24.
+//
+
+import Testing
+@testable import Weathers
+
+struct WeathersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
